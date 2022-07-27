526cd79b3aa559dd3cf7ac6ebb096e406201aa46
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-26 21:50:59 -0300

    feat: adding home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig news</title>
+        <title>Home | ig news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about <br/> 
+            the <span> React</span> world
+          </h1>
+          <p> 
+            Get access to all the publications <br/>
+            for <span>$9.90 monthly</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
