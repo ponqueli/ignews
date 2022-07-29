ba5667a4401832f39476f9bc3b1fcb59d2b0b16c
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-29 17:57:49 -0300

    feat: saving user in faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,7 +3,6 @@ import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
-
 export function SignInButton(){
   const { data: session, status } = useSession()
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,12 @@
+import { signIn } from 'next-auth/react';
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+
 export default NextAuth({
+  secret: process.env.SIGNIN_KEY,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -14,4 +20,42 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+      
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data : { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+        return true;
+      }catch(e){
+        console.log(e);
+        return false;
+      }
+    },
+  },
 })
+
+// FaunaDB - HTTP (vamos usar)
+// DynamoDB - AWS
