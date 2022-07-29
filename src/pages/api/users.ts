88e073351f2ea059dd6013929d0973bf05de472b
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-28 22:04:26 -0300

    feat: understanding api roots and serverless concept

---
+++ src/pages/api/users.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next"
+export default (request:NextApiRequest, response:NextApiResponse) => {
+  const users = [
+    { id: "1", name: "Gaia" },
+    { id: "2", name: "Jose" },
+    { id: "3", name: "Sophia" },
+  ];
+  return response.json(users);
+}
+
+// serverless
+// sobe um ambiente isolado, executar a funcao, retornar a resposta e o ambiente morre
+// não precisa do servidor 24h rodando pra aguardar as requisições
+// sobe e desce conforme a necessidade das chamadas
