135976493d922fe2f6955565bd191a75c1fa6dbd
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-29 06:15:56 -0300

    feat: understanding next parameterization

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default (request:NextApiRequest, response:NextApiResponse) => {
+  console.log(request.query)
+  
+  const users = [
+    { id: "1", name: "Gaia" },
+    { id: "2", name: "Jose" },
+    { id: "3", name: "Sophia" },
+  ];
+  return response.json(users);
+}
