162b3b207d99ef159f794d47bd72c416b0754b87
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-29 08:51:19 -0300

    feat: adding neathauth with github option

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,32 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
+
 export function SignInButton(){
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const { data: session, status } = useSession()
+
+  console.log(session)
+  
+  return status  === "authenticated" ? (
     <button 
       type="button"
       className={styles.signInButton}
       >
       <FaGithub color="#04d361"/>
-      José Contó
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX 
+        color="#737380" 
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+        />
     </button>  
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
       >
       <FaGithub color="#eba417"/>
       Sign in with Github
