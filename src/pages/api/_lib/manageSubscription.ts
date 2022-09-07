0947123db7ff983fbed7a84f73fa2d59009ef159
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-09-06 23:25:30 -0300

    feat: savesubscription

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,9 @@
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string
+){
+  console.log(subscriptionId, customerId);
+  // Find user in FaunaDB with customerId
+
+  // Save subscriptionId to user in FaunaDB
+}
