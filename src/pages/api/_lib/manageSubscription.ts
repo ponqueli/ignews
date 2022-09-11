a3c57a3c6f7cdd78c8584aad36affd9af535c78a
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-09-11 11:32:31 -0300

    saving subscriptions not working yet

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,9 +1,38 @@
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
+
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string
 ){
-  console.log(subscriptionId, customerId);
   // Find user in FaunaDB with customerId
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  )
 
+  // get subscription data from stripe
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
   // Save subscriptionId to user in FaunaDB
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
 }
