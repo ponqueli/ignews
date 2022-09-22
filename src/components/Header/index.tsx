f582850ad73cb3be43a294edffaf3eab8b10204e
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-09-21 21:29:53 -0300

    feat: adjusting header and cloning element

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
+import { useRouter } from "next/router";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
+  const { asPath } = useRouter();
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         {/* imagens sempre na pasta public */}
         <img src="/images/logo.svg" alt="logo" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+        <ActiveLink activeClassName={styles.active} href="/">
+          <a className={asPath==='/' ? styles.active : ''}>Home</a>
+        </ActiveLink>
+        <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+          <a className={asPath==='/posts' ? styles.active : ''}>Posts</a>
+        </ActiveLink>
         </nav>
         <SignInButton />
       </div>
